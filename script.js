034097d88121bd4abbdefcaba296fbe601e2bbd7
Jenina <jeninadelacruz521@yahoo.com>
2024-03-24 03:14:58 -0700

    consolidated and organized javascript

--- script.js
+++ script.js
@@ -15,30 +15,13 @@ var day2C = document.getElementById('d2Content');
 var day3C = document.getElementById('d3Content');
 var day4C = document.getElementById('d4Content');
 var day5C = document.getElementById('d5Content');
-
-
 var apiKey = "1b13cd363449193c96068b99f99b527b"
 cityname = document.getElementById('userInput').value;
-// console.log(cityname)
 
 var today = dayjs()
 console.log(today)
 date.textContent = dayjs().format('  (' + 'MM' + '/' + 'D' + '/' + 'YYYY' + ')')
 day1Text = today.add(1, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY');
-
-
-
-
-
-
-
-
-const unixTimestamp = dayjs().unix();
-
-console.log(unixTimestamp);
-
-
-
 
 function fetchUrl(){
 var apiKey = "1b13cd363449193c96068b99f99b527b"
@@ -58,16 +41,9 @@ fetch(url)
     tempText.textContent = 'Temperature: ' + temperature + ' °F'
     windText.textContent = 'Wind: ' + data.wind.speed + ' MPH'
     humidText.textContent = 'Humidity: ' + data.main.humidity + ' %'
-    //const location = data.name;
-//     outputElement.innerHTML = `<p>Temperature in ${location}: ${temperature}°C</p>
-//                                <p>Weather: ${description}</p>`;
-// console.log(location)
   })
 
 }
-
-
-
 
 function fetch5Day(){
 var cityName = document.getElementById('userInput').value;
@@ -90,146 +66,67 @@ console.log(nextD)
 samedayArray = result.list.filter(list => list.dt_txt.startsWith(nextD))
 console.log(samedayArray)
 daysArray.push(samedayArray)
-// temp = result.list.filter(list => list.main.temp)
-// console.log(temp)
 
-
-
-
-//START OF ORIGINAL HERE
-// nextD2 = today.add(2, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD2)
-// sameday2 = result.list.filter(list => list.dt_txt.startsWith(nextD2))
-// console.log(sameday2)
-
-// nextD3 = today.add(3, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD3)
-// sameday3 = result.list.filter(list => list.dt_txt.startsWith(nextD3))
-// console.log(sameday3)
-
-// nextD4 = today.add(4, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD4)
-// sameday4 = result.list.filter(list => list.dt_txt.startsWith(nextD4))
-// console.log(sameday4)
-
-// nextD5 = today.add(5, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD5)
-// sameday5 = result.list.filter(list => list.dt_txt.startsWith(nextD5))
-// console.log(sameday5)
 }
 console.log(daysArray)
 console.log(daysArray.length)
-temp1 = daysArray[2]
-console.log(temp1)
+day1 = daysArray[0]
+console.log(day1)
+day2 = daysArray[1]
+day3 = daysArray[2]
+day4 = daysArray[3]
+day5 = daysArray[4]
 
 
-for(var i = 0; i < daysArray.length; i++){
-  perDay = daysArray[i]
-  //perdayTemp = perDay.main.temp
-//console.log(perdayTemp)
-console.log(perDay.length)
+tempList1 = []; windList1 = []; humidList1 = [];
+for (var i = 0; i < day1.length; i++){
+  tempList1.push(day1[i].main.temp)
+  windList1.push(day1[i].wind.speed)
+  humidList1.push(day1[i].main.humidity)
+}
+maxTemp1 = Math.max(...tempList1)
+maxWind1 = Math.max(...windList1)
+maxhumid1 = Math.max(...humidList1)
 
+tempList2 = []; windList2 = []; humidList2 = []
+tempList3 = []; windList3 = []; humidList3 = []
+tempList4 = []; windList4 = []; humidList4 = []
+for (var i = 0; i < day2.length; i++){
+  tempList2.push(day2[i].main.temp)
+  windList2.push(day2[i].wind.speed)
+  humidList2.push(day2[i].main.humidity)
 
+  tempList3.push(day3[i].main.temp)
+  windList3.push(day3[i].wind.speed)
+  humidList3.push(day3[i].main.humidity)
 
-
-
+  tempList4.push(day4[i].main.temp)
+  windList4.push(day4[i].wind.speed)
+  humidList4.push(day4[i].main.humidity)
 }
 
-for(var i = 0; i < perDay.length; i++){
-  day1Temp = perDay[i].main.temp
-  console.log(day1Temp)
+maxTemp2 = Math.max(...tempList2)
+maxWind2 = Math.max(...windList2)
+maxhumid2 = Math.max(...humidList2)
 
+maxTemp3 = Math.max(...tempList3)
+maxWind3 = Math.max(...windList3)
+maxhumid3 = Math.max(...humidList3)
+
+maxTemp4= Math.max(...tempList4)
+maxWind4 = Math.max(...windList4)
+maxhumid4 = Math.max(...humidList4)
+
+tempList5 = []; windList5 = []; humidList5 = []
+for (var i = 0; i < day5.length; i++){
+  tempList5.push(day5[i].main.temp)
+  windList5.push(day5[i].wind.speed)
+  humidList5.push(day5[i].main.humidity)
 }
+maxTemp5 = Math.max(...tempList5)
+maxWind5 = Math.max(...windList5)
+maxhumid5 = Math.max(...humidList5)
 
-
-
-// nextD = today.add(1, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD)
-// sameday1 = result.list.filter(list => list.dt_txt.startsWith(nextD))
-// console.log(sameday1)
-// temp1 = result.list.filter(list => list.main.temp)
-
-// nextD2 = today.add(2, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD2)
-// sameday2 = result.list.filter(list => list.dt_txt.startsWith(nextD2))
-// console.log(sameday2)
-
-// nextD3 = today.add(3, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD3)
-// sameday3 = result.list.filter(list => list.dt_txt.startsWith(nextD3))
-// console.log(sameday3)
-
-// nextD4 = today.add(4, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD4)
-// sameday4 = result.list.filter(list => list.dt_txt.startsWith(nextD4))
-// console.log(sameday4)
-
-// nextD5 = today.add(5, 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
-// console.log(nextD5)
-// sameday5 = result.list.filter(list => list.dt_txt.startsWith(nextD5))
-// console.log(sameday5)
-
-var temp1 = [sameday1[0].main.temp]
-var wind1 = [sameday1[0].wind.speed]
-var humid1 = [sameday1[0].main.humidity]
-for (var i = 1; i < sameday1.length; i++){
-  temp1.push(sameday1[i].main.temp)
-  wind1.push(sameday1[i].wind.speed)
-  humid1.push(sameday1[i].main.humidity)
-}
-maxTemp1 = Math.max(...temp1)
-maxWind1 = Math.max(...wind1)
-maxhumid1 = Math.max(...humid1)
-
-var temp2 = [sameday2[0].main.temp]
-var wind2 = [sameday2[0].wind.speed]
-var humid2 = [sameday2[0].main.humidity]
-for (var i = 1; i < sameday2.length; i++){
-  temp2.push(sameday2[i].main.temp)
-  wind2.push(sameday2[i].wind.speed)
-  humid2.push(sameday2[i].main.humidity)
-}
-maxTemp2 = Math.max(...temp2)
-maxWind2 = Math.max(...wind2)
-maxhumid2 = Math.max(...humid2)
-
-var temp3 = [sameday3[0].main.temp]
-var wind3 = [sameday3[0].wind.speed]
-var humid3 = [sameday3[0].main.humidity]
-for (var i = 1; i < sameday3.length; i++){
-  temp3.push(sameday3[i].main.temp)
-  wind3.push(sameday3[i].wind.speed)
-  humid3.push(sameday3[i].main.humidity)
-}
-maxTemp3 = Math.max(...temp3)
-maxWind3 = Math.max(...wind3)
-maxhumid3 = Math.max(...humid3)
-
-var temp4 = [sameday4[0].main.temp]
-var wind4 = [sameday4[0].wind.speed]
-var humid4 = [sameday4[0].main.humidity]
-for (var i = 1; i < sameday4.length; i++){
-  temp4.push(sameday4[i].main.temp)
-  wind4.push(sameday4[i].wind.speed)
-  humid4.push(sameday4[i].main.humidity)
-}
-maxTemp4 = Math.max(...temp4)
-maxWind4 = Math.max(...wind4)
-maxhumid4 = Math.max(...humid4)
-
-var temp5 = [sameday5[0].main.temp]
-var wind5 = [sameday5[0].wind.speed]
-var humid5 = [sameday5[0].main.humidity]
-for (var i = 1; i < sameday5.length; i++){
-  temp5.push(sameday5[i].main.temp)
-  wind5.push(sameday5[i].wind.speed)
-  humid5.push(sameday5[i].main.humidity)
-}
-maxTemp5 = Math.max(...temp5)
-maxWind5 = Math.max(...wind5)
-maxhumid5 = Math.max(...humid5)
-
- 
 day1Div.textContent = day1Text + "\n \nTemp: " + maxTemp1 + " °F" + "\n Wind: " + maxWind1 + "\n Humidity: " + maxhumid1;
 day2Div.textContent = today.add(2, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp2 + " °F" + "\n Wind: " + maxWind2 + "\n Humidity: " + maxhumid2;                     
 day3Div.textContent = today.add(3, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp3 + " °F" + "\n Wind: " + maxWind3 + "\n Humidity: " + maxhumid3;
