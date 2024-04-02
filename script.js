5055807217df9e8ba746cd48300d6c29cad55d52
Jenina <jeninadelacruz521@yahoo.com>
2024-04-02 03:13:36 -0700

    made list stay after loading

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ var day4C = document.getElementById('d4Content');
 var day5C = document.getElementById('d5Content');
 var historyList = document.getElementById('historyList');
 var iconElement = document.getElementById('iconEl');
-var iconElement1 = document.getElementById('iconEl1');
-
+var icon1 = document.getElementById('icon1');
 var apiKey = "1b13cd363449193c96068b99f99b527b"
 cityname = document.getElementById('userInput').value;
+console.log(cityname)
 var today = dayjs()
   console.log(today)
+var url  = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=' + apiKey + '&units=imperial';
 
 
 function fetchUrl(){
@@ -42,7 +43,6 @@ fetch(url)
     windText.textContent = 'Wind: ' + data.wind.speed + ' MPH'
     humidText.textContent = 'Humidity: ' + data.main.humidity + ' %'
 
-
     iconElement.textContent = ''
     iconEl = data.weather[0].icon;
     console.log(iconEl)
@@ -53,17 +53,9 @@ fetch(url)
     iconElement.appendChild(imageEl)
   })
  
-  
-
-
-
   date.textContent = dayjs().format('  (' + 'MM' + '/' + 'D' + '/' + 'YYYY' + ')')
   day1Text = today.add(1, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY');
 }
-
-
-
-
 
 function fetch5Day(){
 var cityName = document.getElementById('userInput').value;
@@ -80,11 +72,10 @@ console.log(result)
 
 daysArray = []
 for (var i = 1; i <= 5; i++){
-    nextD = today.add([i], 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
+  nextD = today.add([i], 'day').format('YYYY' + '-' + 'MM' + '-' + 'DD')
     
-samedayArray = result.list.filter(list => list.dt_txt.startsWith(nextD))
-daysArray.push(samedayArray)
-
+  samedayArray = result.list.filter(list => list.dt_txt.startsWith(nextD))
+  daysArray.push(samedayArray)
 
 }
 day1 = daysArray[0]
@@ -92,32 +83,18 @@ day2 = daysArray[1]
 day3 = daysArray[2]
 day4 = daysArray[3]
 day5 = daysArray[4]
-
-  // // iconElement1.textContent = ''
-  // iconEl1 = day1[0].weather[0].icon;
-  // console.log(iconEl1)
-  // iconUrl1 = 'https://openweathermap.org/img/wn/' + iconEl1 + '.png'
-  // imageEl1 = document.createElement('img')
-  // imageEl1.src = iconUrl1
-  // iconElement1.appendChild(imageEl1)
-
-
-
-
-console.log(day1)
 tempList1 = []; windList1 = []; humidList1 = [];
+
 for (var i = 0; i < day1.length; i++){
   tempList1.push(day1[i].main.temp)
   windList1.push(day1[i].wind.speed)
   humidList1.push(day1[i].main.humidity)
-  
-
-
 }
+console.log(day4)
+
 maxTemp1 = Math.max(...tempList1)
 maxWind1 = Math.max(...windList1)
 maxhumid1 = Math.max(...humidList1)
-
 
 tempList2 = []; windList2 = []; humidList2 = []
 tempList3 = []; windList3 = []; humidList3 = []
@@ -158,6 +135,13 @@ maxTemp5 = Math.max(...tempList5)
 maxWind5 = Math.max(...windList5)
 maxhumid5 = Math.max(...humidList5)
 
+iconEl1 = day1[2].weather[0].icon;
+console.log(iconEl)
+iconUrl = 'https://openweathermap.org/img/wn/' + iconEl1 + '.png'
+imageEl1 = document.createElement('img')
+imageEl.src = iconUrl
+day1Icon = icon1.appendChild(imageEl1)
+
 day1Div.textContent = day1Text + "\n \nTemp: " + maxTemp1 + " °F" + "\n Wind: " + maxWind1 + "\n Humidity: " + maxhumid1;
 day2Div.textContent = today.add(2, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp2 + " °F" + "\n Wind: " + maxWind2 + "\n Humidity: " + maxhumid2;                     
 day3Div.textContent = today.add(3, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp3 + " °F" + "\n Wind: " + maxWind3 + "\n Humidity: " + maxhumid3;
@@ -166,177 +150,268 @@ day5Div.textContent = today.add(5, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY'
 }
 )
 }
-
+ 
+var listOfCity = [];
+var storedList = JSON.parse(localStorage.getItem('listOfCity'))
+if(!storedList){
+  storedList = [];
+}
 
 buttonEl.addEventListener('click', function(){
-fetchUrl()
-fetch5Day()
-city.textContent = cityname.toUpperCase()
-myCity = city.textContent
-console.log(typeof myCity)
-setLocal()
-getLocal()
+  //starts here
 
-})
-
-
-
-
-
-//Setting to local storage
-function setLocal(){
-  setCity = localStorage.setItem('my city', myCity)
-  setArray = []
-  setArray.push(setCity)
-  setCityarray = localStorage.setItem('my cityA', setArray)
-  console.log(setArray)
+  storedList.push(cityname)
+  localStorage.setItem('listOfCity', JSON.stringify(storedList))
+  console.log(storedList)
+  console.log(cityname)
+  renderList()  
+  //ends here
+  fetchUrl()
+  fetch5Day()
+  city.textContent = cityname.toUpperCase()
 }
-
-function getLocal (){
-  getCity = localStorage.getItem('my city')
-  getCityarray = localStorage.getItem('my cityA')
-
-createLi()
-}
-
-function createLi(){
-  liEl = document.createElement('li')
-  liEl.innerHTML = getCity
-for(var i = 0; i > 0; i++){
-}
-historyList.appendChild(liEl);
-
-liEl.addEventListener("click", function(event){
-  date.textContent = dayjs().format('  (' + 'MM' + '/' + 'D' + '/' + 'YYYY' + ')')
-  day1Text = today.add(1, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY');
-  newCity = event.target.textContent
-  console.log(newCity)
-  var apiKey = "1b13cd363449193c96068b99f99b527b"
-  cityname = newCity;
-  var url  = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=' + apiKey + '&units=imperial';
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+)
+ 
+function renderList(){
+  historyList.innerHTML ="";
+  for (var i=0; i<storedList.length; i++ ) {
+    var listObj = storedList[i];
+    liEl = document.createElement('li')
+    liEl.classList.add('liClass');
+    liEl.textContent = listObj.toUpperCase()
+    //liEl.innerHTML = cityname.toUpperCase()
+  // for(var i = 0; i > 0; i++){
+  // }
+    historyList.appendChild(liEl);
+  
+      //     var btn = document.createElement("button");
+  //     btn.textContent = listObj;
+  //     btn.setAttribute("data-index", i);
+  
+  //     list.appendChild(btn);
+  
+    liEl.addEventListener("click", function(event){
+      date.textContent = dayjs().format('  (' + 'MM' + '/' + 'D' + '/' + 'YYYY' + ')')
+      day1Text = today.add(1, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY');
+      newCity = event.target.textContent
+      console.log(newCity)
+      var apiKey = "1b13cd363449193c96068b99f99b527b"
+      cityname = newCity;
+      var url  = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=' + apiKey + '&units=imperial';
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+          throw new Error('Network response was not ok');
+          }
+          return response.json();
         }
-        return response.json();
-      })
-    .then(data => {
-      city.textContent = cityname.toUpperCase()
-      var temperature = data.main.temp;
-      tempText.textContent = 'Temperature: ' + temperature + ' °F'
-      windText.textContent = 'Wind: ' + data.wind.speed + ' MPH'
-      humidText.textContent = 'Humidity: ' + data.main.humidity + ' %'
-      iconElement.textContent = ''
-      iconEl = data.weather[0].icon;
-      iconUrl = 'https://openweathermap.org/img/wn/' + iconEl + '.png'
-      imageEl = document.createElement('img')
-      imageEl.src = iconUrl
-      iconElement.appendChild(imageEl)
-      })
-    
-  var fivedayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=' + apiKey + '&units=imperial';
-  fetch(fivedayUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-        return response.json();
-    })
-    .then(result => {
-      daysArray = []
+        )
+        .then(data => {
+          city.textContent = cityname.toUpperCase()
+          var temperature = data.main.temp;
+          tempText.textContent = 'Temperature: ' + temperature + ' °F'
+          windText.textContent = 'Wind: ' + data.wind.speed + ' MPH'
+          humidText.textContent = 'Humidity: ' + data.main.humidity + ' %'
+          iconElement.textContent = ''
+          iconEl = data.weather[0].icon;
+          iconUrl = 'https://openweathermap.org/img/wn/' + iconEl + '.png'
+          imageEl = document.createElement('img')
+          imageEl.src = iconUrl
+          iconElement.appendChild(imageEl)
+        }
+        )
+      
+var fivedayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=' + apiKey + '&units=imperial';
+
+fetch(fivedayUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+      return response.json();
+  }
+  )
+  .then(result => {
+    daysArray = []
       for (var i = 1; i <= 5; i++){
-        nextD = today.add([i], 'day').format('YYYY' + '-' + 'MM' + '-' + 'D')
+        nextD = today.add([i], 'day').format('YYYY' + '-' + 'MM' + '-' + 'DD')
         samedayArray = result.list.filter(list => list.dt_txt.startsWith(nextD))
         daysArray.push(samedayArray)
       }
-  day1 = daysArray[0]
-  day2 = daysArray[1]
-  day3 = daysArray[2]
-  day4 = daysArray[3]
-  day5 = daysArray[4]
-
-  // // iconElement1.textContent = ''
-  // iconEl1 = day1[0].weather[0].icon;
-  // console.log(iconEl1)
-  // iconUrl1 = 'https://openweathermap.org/img/wn/' + iconEl1 + '.png'
-  // imageEl1 = document.createElement('img')
-  // imageEl1.src = iconUrl1
-  // iconElement1.appendChild(imageEl1)
-
-
-
-
-console.log(day1)
-tempList1 = []; windList1 = []; humidList1 = [];
-for (var i = 0; i < day1.length; i++){
-  tempList1.push(day1[i].main.temp)
-  windList1.push(day1[i].wind.speed)
-  humidList1.push(day1[i].main.humidity)
+    day1 = daysArray[0]
+    day2 = daysArray[1]
+    day3 = daysArray[2]
+    day4 = daysArray[3]
+    day5 = daysArray[4]
   
-
-
+    // // iconElement1.textContent = ''
+    // iconEl1 = day1[0].weather[0].icon;
+    // console.log(iconEl1)
+    // iconUrl1 = 'https://openweathermap.org/img/wn/' + iconEl1 + '.png'
+    // imageEl1 = document.createElement('img')
+    // imageEl1.src = iconUrl1
+    // iconElement1.appendChild(imageEl1)
+  console.log(day1)
+  tempList1 = []; windList1 = []; humidList1 = [];
+  for (var i = 0; i < day1.length; i++){
+    tempList1.push(day1[i].main.temp)
+    windList1.push(day1[i].wind.speed)
+    humidList1.push(day1[i].main.humidity)
+  }
+  maxTemp1 = Math.max(...tempList1)
+  maxWind1 = Math.max(...windList1)
+  maxhumid1 = Math.max(...humidList1)
+  
+  tempList2 = []; windList2 = []; humidList2 = []
+  tempList3 = []; windList3 = []; humidList3 = []
+  tempList4 = []; windList4 = []; humidList4 = []
+  for (var i = 0; i < day2.length; i++){
+    tempList2.push(day2[i].main.temp)
+    windList2.push(day2[i].wind.speed)
+    humidList2.push(day2[i].main.humidity)
+  
+    tempList3.push(day3[i].main.temp)
+    windList3.push(day3[i].wind.speed)
+    humidList3.push(day3[i].main.humidity)
+  
+    tempList4.push(day4[i].main.temp)
+    windList4.push(day4[i].wind.speed)
+    humidList4.push(day4[i].main.humidity)
+  }
+  
+  maxTemp2 = Math.max(...tempList2)
+  maxWind2 = Math.max(...windList2)
+  maxhumid2 = Math.max(...humidList2)
+  
+  maxTemp3 = Math.max(...tempList3)
+  maxWind3 = Math.max(...windList3)
+  maxhumid3 = Math.max(...humidList3)
+  
+  maxTemp4= Math.max(...tempList4)
+  maxWind4 = Math.max(...windList4)
+  maxhumid4 = Math.max(...humidList4)
+  
+  tempList5 = []; windList5 = []; humidList5 = []
+  for (var i = 0; i < day5.length; i++){
+    tempList5.push(day5[i].main.temp)
+    windList5.push(day5[i].wind.speed)
+    humidList5.push(day5[i].main.humidity)
+  }
+  maxTemp5 = Math.max(...tempList5)
+  maxWind5 = Math.max(...windList5)
+  maxhumid5 = Math.max(...humidList5)
+  
+  day1Div.textContent = day1Text + "\n \nTemp: " + maxTemp1 + " °F" + "\n Wind: " + maxWind1 + "\n Humidity: " + maxhumid1;
+  day2Div.textContent = today.add(2, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp2 + " °F" + "\n Wind: " + maxWind2 + "\n Humidity: " + maxhumid2;                     
+  day3Div.textContent = today.add(3, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp3 + " °F" + "\n Wind: " + maxWind3 + "\n Humidity: " + maxhumid3;
+  day4Div.textContent = today.add(4, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp4 + " °F" + "\n Wind: " + maxWind4 + "\n Humidity: " + maxhumid4;
+  day5Div.textContent = today.add(5, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp5 + " °F" + "\n Wind: " + maxWind5 + "\n Humidity: " + maxhumid5;
+  }
+  )
+  })
 }
-maxTemp1 = Math.max(...tempList1)
-maxWind1 = Math.max(...windList1)
-maxhumid1 = Math.max(...humidList1)
-
-
-tempList2 = []; windList2 = []; humidList2 = []
-tempList3 = []; windList3 = []; humidList3 = []
-tempList4 = []; windList4 = []; humidList4 = []
-for (var i = 0; i < day2.length; i++){
-  tempList2.push(day2[i].main.temp)
-  windList2.push(day2[i].wind.speed)
-  humidList2.push(day2[i].main.humidity)
-
-  tempList3.push(day3[i].main.temp)
-  windList3.push(day3[i].wind.speed)
-  humidList3.push(day3[i].main.humidity)
-
-  tempList4.push(day4[i].main.temp)
-  windList4.push(day4[i].wind.speed)
-  humidList4.push(day4[i].main.humidity)
 }
-
-maxTemp2 = Math.max(...tempList2)
-maxWind2 = Math.max(...windList2)
-maxhumid2 = Math.max(...humidList2)
-
-maxTemp3 = Math.max(...tempList3)
-maxWind3 = Math.max(...windList3)
-maxhumid3 = Math.max(...humidList3)
-
-maxTemp4= Math.max(...tempList4)
-maxWind4 = Math.max(...windList4)
-maxhumid4 = Math.max(...humidList4)
-
-tempList5 = []; windList5 = []; humidList5 = []
-for (var i = 0; i < day5.length; i++){
-  tempList5.push(day5[i].main.temp)
-  windList5.push(day5[i].wind.speed)
-  humidList5.push(day5[i].main.humidity)
-}
-maxTemp5 = Math.max(...tempList5)
-maxWind5 = Math.max(...windList5)
-maxhumid5 = Math.max(...humidList5)
-
-day1Div.textContent = day1Text + "\n \nTemp: " + maxTemp1 + " °F" + "\n Wind: " + maxWind1 + "\n Humidity: " + maxhumid1;
-day2Div.textContent = today.add(2, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp2 + " °F" + "\n Wind: " + maxWind2 + "\n Humidity: " + maxhumid2;                     
-day3Div.textContent = today.add(3, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp3 + " °F" + "\n Wind: " + maxWind3 + "\n Humidity: " + maxhumid3;
-day4Div.textContent = today.add(4, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp4 + " °F" + "\n Wind: " + maxWind4 + "\n Humidity: " + maxhumid4;
-day5Div.textContent = today.add(5, 'day').format('MM' + '/' + 'D' + '/' + 'YYYY') + "\n Temp: " + maxTemp5 + " °F" + "\n Wind: " + maxWind5 + "\n Humidity: " + maxhumid5;
-}
-)
-
-
-})
+renderList()
 
 
 
-}
+
+  //startshere
+  // searchBtn.addEventListener('click', function(){
+  //   var cityName = document.getElementById('userInput').value
+  // var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
+  
+  // var resultPage = document.getElementById('resultPage'); 
+  
+  // storedList.push(cityName)
+  // localStorage.setItem('listOfCity', JSON.stringify(storedList))
+  // renderList()
+  //   } )
+  //   var list = document.getElementById('listOfCity')
+  //   var listOfCity = [];
+  
+  //   function renderList(){
+  //     list.innerHTML ="";
+    
+  //     for (var i=0; i<storedList.length; i++ ) {
+  //     var listObj = storedList[i];
+  
+  //     var btn = document.createElement("button");
+  //     btn.textContent = listObj;
+  //     btn.setAttribute("data-index", i);
+  
+  //     list.appendChild(btn);
+  //     }}
+  
+  // renderList()
+  //ends here
 
 
 
-getLocal()
+
+
+
+
+
+
+//starts here
+//   var storedList = JSON.parse(localStorage.getItem('listOfCity'))
+//   if(!storedList){
+//     storedList = [];
+//   }
+//   var storedList = JSON.parse(localStorage.getItem('listOfCity'))
+// if(!storedList){
+//   storedList = [];
+// }
+// console.log(storedList)
+
+// searchBtn.addEventListener('click', function(){
+//   var cityName = document.getElementById('userInput').value
+// var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
+
+// var resultPage = document.getElementById('resultPage'); 
+
+// storedList.push(cityName)
+// localStorage.setItem('listOfCity', JSON.stringify(storedList))
+// renderList()
+//   } )
+//   var list = document.getElementById('listOfCity')
+//   var listOfCity = [];
+
+//   function renderList(){
+//     list.innerHTML ="";
+  
+//     for (var i=0; i<storedList.length; i++ ) {
+//     var listObj = storedList[i];
+
+//     var btn = document.createElement("button");
+//     btn.textContent = listObj;
+//     btn.setAttribute("data-index", i);
+
+//     list.appendChild(btn);
+//     }}
+
+// renderList()
+
+// //ends here
+
+
+
+
+
+
+// }
+
+
+// function getLocal (){
+//   getCity = localStorage.getItem('my city')
+
+//   //createLi()
+// }
+
+// function createLi(){
+  
+// }
+
+//getLocal()
 
